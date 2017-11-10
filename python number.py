519c7a0a8dfeecd91ee954fa0a56a41793256fa5
zhucuicuia <33245616+zhucuicuia@users.noreply.github.com>
2017-11-10 09:46:18 +0800

    Add files via upload

---
+++ python number.py
@@ -0,0 +1,17 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> tuple1=('a', 'c', 'e')
+>>> list(tuple1)
+['a', 'c', 'e']
+>>> tuple2 = (123, 'a', 'b')
+>>> list(tuple2)
+[123, 'a', 'b']
+>>> tuple3 = (1, 2, 3 ,4)
+>>> list(tuple2)
+[123, 'a', 'b']
+>>> list(tuple3)
+[1, 2, 3, 4]
+>>> tuple1 = (1,2,3,4)
+>>> list(tuple1)
+[1, 2, 3, 4]
+>>> 
